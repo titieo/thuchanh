8bf547f8060314dc4683c42afc7fc3ab2ee5687a
ItsBioDude <locphuc016@gmail.com>
2023-05-13 21:13:41 +0700

    ci: test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import UnoCSS from 'unocss/vite';
 export default defineConfig({
+	server: {
+		open: true,
+	},
 	plugins: [
 		UnoCSS({
 			extractors: [
