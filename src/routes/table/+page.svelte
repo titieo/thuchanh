bbb83d9a85f6c05fb22f5f714a43361deac169da
ItsBioDude <locphuc016@gmail.com>
2023-07-06 14:58:47 +0700

    misc fix and new feature for table

--- src/routes/table/+page.svelte
+++ src/routes/table/+page.svelte
@@ -1,7 +1,4 @@
 <script>
-	import { createTable, Subscribe, Render } from 'svelte-headless-table';
-	import { readable } from 'svelte/store';
-	// import OpenMenu from '$lib/OpenMenu.svelte';
 	export let data;
 
 	let { dates } = data;
@@ -16,67 +13,35 @@
 		lhs: ele.lhs === null ? '' : ele.lhs,
 		seven_lines: ele.seven_lines === null ? '' : ele.seven_lines,
 	}));
-	const tableData = readable(inputDates);
 
-	const table = createTable(tableData);
-	const headerLabel = [
-		'Thời gian',
-		'Green Tara',
-		'21 Tara Homages',
-		'Lạy Dài',
-		'Kiết Già Hàng Phục',
-		'Guru Rinpoche',
-		'Seven Lines Guru Rinpoche',
-	];
-	const headerAncesor = [
-		'created_at',
-		'tara',
-		'taras_homage',
-		'lay_dai',
-		'hang_phuc',
-		'lhs',
-		'seven_lines',
-	];
+	let sum = {
+		title: 'Sum',
+		tara: 0,
+		taras_homage: 0,
+		lay_dai: 'N/A',
+		hang_phuc: 'N/A',
+		lhs: 0,
+		seven_lines: 0,
+	};
 
-	const columns = table.createColumns([
-		// headerLabel.map(label, (i) =>
-		// 	table.column({
-		// 		header: label,
-		// 		accessor: headerAncesor[i],
-		// 	})
-		// )
-		table.column({
-			header: headerLabel[0],
-			accessor: headerAncesor[0],
-		}),
-		table.column({
-			header: headerLabel[1],
-			accessor: headerAncesor[1],
-		}),
-		table.column({
-			header: headerLabel[2],
-			accessor: headerAncesor[2],
-		}),
-		table.column({
-			header: headerLabel[3],
-			accessor: headerAncesor[3],
-		}),
-		table.column({
-			header: headerLabel[4],
-			accessor: headerAncesor[4],
-		}),
-		table.column({
-			header: headerLabel[5],
-			accessor: headerAncesor[5],
-		}),
-		table.column({
-			header: headerLabel[6],
-			accessor: headerAncesor[6],
-		}),
-	]);
+	dates.forEach((ele) => {
+		sum.tara += ele.tara;
+		sum.taras_homage += ele.taras_homage;
+		sum.lhs += ele.lhs;
+		sum.seven_lines += ele.seven_lines;
+	});
+	// const sum = dates.map((ele) => {});
+	const headers = Object.entries({
+		created_at: 'Ngày',
+		tara: 'Green Tara',
+		taras_homage: '21 Tara Homages',
+		lay_dai: 'Lạy Dài',
+		hang_phuc: 'Kiết Già Hàng Phục',
+		lhs: 'Guru Rinpoche',
+		seven_lines: 'Seven Lines Guru Rinpoche',
+	});
 
-	const { headerRows, rows, tableAttrs, tableBodyAttrs } =
-		table.createViewModel(columns);
+	console.log(Object.entries(dates));
 </script>
 
 <svelte:head>
@@ -91,43 +56,46 @@
 	>
 		<div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
 			<div class="overflow-hidden">
-				<table
-					{...$tableAttrs}
-					class="rounded-md min-w-full text-left text-sm font-light"
-				>
+				<table class="rounded-md min-w-full text-left text-sm font-light">
 					<thead
 						class="border-b font-medium font-title text-lg dark:border-neutral-500"
 					>
-						{#each $headerRows as headerRow (headerRow.id)}
-							<Subscribe rowAttrs={headerRow.attrs()} let:rowAttrs>
-								<tr {...rowAttrs}>
-									{#each headerRow.cells as cell (cell.id)}
-										<Subscribe attrs={cell.attrs()} let:attrs>
-											<th {...attrs} class="px-6 py-4">
-												<Render of={cell.render()} />
-											</th>
-										</Subscribe>
-									{/each}
-								</tr>
-							</Subscribe>
-						{/each}
+						<tr>
+							{#each headers as [content, title]}
+								<th class="px-6 py-4">
+									{title}
+								</th>{/each}
+						</tr>
 					</thead>
-					<tbody {...$tableBodyAttrs}>
-						{#each $rows as row (row.id)}
-							<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-								<tr
-									{...rowAttrs}
-									class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 text-lg font-normal dark:border-neutral-500 dark:hover:bg-neutral-600"
-								>
-									{#each row.cells as cell (cell.id)}
-										<Subscribe attrs={cell.attrs()} let:attrs>
-											<td class="whitespace-nowrap px-6 py-4" {...attrs}>
-												<Render of={cell.render()} />
-											</td>
-										</Subscribe>
-									{/each}
-								</tr>
-							</Subscribe>
+					<tbody>
+						<tr
+							class="border-b font-medium font-title text-lg dark:border-neutral-500"
+						>
+							{#each Object.entries(sum) as [title, content]}
+								<td class="whitespace-nowrap font-italic px-6 py-4">
+									{#if title == 'tara'}
+										{content} / {Math.ceil(1200000 / 108).toLocaleString('en', {
+											useGrouping: true,
+										})}
+									{:else}
+										{content}{/if}
+								</td>
+							{/each}
+						</tr>
+						{#each inputDates as date (date.id)}
+							<tr
+								class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 text-lg font-normal dark:border-neutral-500 dark:hover:bg-neutral-600"
+							>
+								{#each headers as [content, title]}
+									<td class="whitespace-nowrap px-6 py-4">
+										{#if date[content] === 0 && content === 'tara'}
+											NQ Ngắn
+										{:else}
+											{date[content]}
+										{/if}
+									</td>
+								{/each}
+							</tr>
 						{/each}
 					</tbody>
 				</table>
